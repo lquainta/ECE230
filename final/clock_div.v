fd6d0f8268be511be7ac54e606d22592c0d6d905
Landon Quaintance <156140284+lquainta@users.noreply.github.com>
2025-04-24 14:07:26 -0600

    Update clock_div.v

--- final/clock_div.v
+++ final/clock_div.v
@@ -8,29 +8,23 @@ module clock_div
     output reg div_clock
 );
 
-    wire [16:0] ff_out;
-    wire [16:0] ff_in;
-    
-    assign ff_in[0] = ~ff_out[0];
-    assign ff_in[16:1] = ff_out[15:0];
-    always @ (posedge clock or posedge reset) begin
-        if (reset)
-            div_clock <= 1'b0;
-        else
-            div_clock <= ff_out[16];
-    end
-    
+   
+    wire [DIVIDE_BY-1:0] Q;  
+   
     genvar i;
     generate
-        for (i = 0; i < 17; i = i + 1) begin: divider
-            dff (
-            .clock(clock),
-            .reset(reset),
-            .D(ff_in[i]),
-            .Q(ff_out[i]),
-            .NotQ(~ff_out[i])
-            );
-            end
-     endgenerate    
+        for(i = 0; i < DIVIDE_BY; i = i + 1) begin : gen_clkDff
+            dff clkDff(
+                .reset(reset),
+                .D(~Q[i]),
+                .Q(Q[i]),
+                .clock(i == 0 ? clock : Q[i-1])
+             );
+        end
+    endgenerate
+    always@(posedge clock) begin
+        div_clock <= Q[DIVIDE_BY-1];
+    end
+
 
 endmodule
